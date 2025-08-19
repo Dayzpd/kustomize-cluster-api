da4a4a7a93a8b4c387746341a04643a512b70b79
dayzpd <zach@dayzpd.com>
2025-08-19 07:55:01 -0500

    add caren and enable runtime sdk/cluster topology feature gates

--- scripts/generate-providers.sh
+++ scripts/generate-providers.sh
@@ -7,6 +7,7 @@ coreVersion="v1.9.7"
 capmoxVersion="v0.7.2"
 inClusterIPAMVersion="v1.0.2"
 helmAddonVersion="v0.3.2"
+carenVersion="v0.33.1"
 
 function generateCoreProvider() {
 
@@ -15,16 +16,19 @@ function generateCoreProvider() {
   mkdir -p $coreDir
 
   clusterctl generate provider \
+    --config clusterctl.yaml \
     --target-namespace $namespace \
     --core cluster-api:$coreVersion | \
     yq -y 'select(.kind != "Namespace")' > $coreDir/cluster-api.yaml
 
   clusterctl generate provider \
+    --config clusterctl.yaml \
     --target-namespace $namespace \
     --bootstrap kubeadm:$coreVersion | \
     yq -y 'select(.kind != "Namespace")' > $coreDir/bootstrap.yaml
 
   clusterctl generate provider \
+    --config clusterctl.yaml \
     --target-namespace $namespace \
     --control-plane kubeadm:$coreVersion | \
     yq -y 'select(.kind != "Namespace")' > $coreDir/control-plane.yaml
@@ -46,6 +50,7 @@ function generateProxmoxInfraProvider() {
   mkdir -p $infraDir
 
   clusterctl generate provider \
+    --config clusterctl.yaml \
     --target-namespace $namespace \
     --infrastructure proxmox:$capmoxVersion | \
     yq -y 'select(.kind != "Namespace")' > $infraDir/infrastructure.yaml
@@ -67,6 +72,7 @@ function generateInClusterIPAMProvider() {
   mkdir -p $ipamDir
 
   clusterctl generate provider \
+    --config clusterctl.yaml \
     --target-namespace $namespace \
     --ipam in-cluster:$inClusterIPAMVersion | \
     yq -y 'select(.kind != "Namespace")' > $ipamDir/ipam.yaml
@@ -88,6 +94,7 @@ function generateHelmAddonProvider() {
   mkdir -p $helmDir
 
   clusterctl generate provider \
+    --config clusterctl.yaml \
     --target-namespace $namespace \
     --addon helm:$helmAddonVersion | \
     yq -y 'select(.kind != "Namespace")' > $helmDir/helm.yaml
@@ -102,6 +109,28 @@ function generateHelmAddonProvider() {
 
 }
 
+function generateCARENProvider() {
+
+  carenDir="components/caren/$carenVersion"
+
+  mkdir -p $carenDir
+
+  clusterctl generate provider \
+    --config clusterctl.yaml \
+    --target-namespace $namespace \
+    --runtime-extension caren:$carenVersion | \
+    yq -y 'select(.kind != "Namespace")' > $carenDir/caren.yaml
+
+  cd $carenDir
+
+  rm -f kustomization.yaml
+
+  kustomize create --resources caren.yaml
+
+  cd $currentDir
+
+}
+
 function main() {
 
   generateCoreProvider
@@ -111,6 +140,8 @@ function main() {
   generateInClusterIPAMProvider
 
   generateHelmAddonProvider
+
+  generateCARENProvider
 
 }
 
